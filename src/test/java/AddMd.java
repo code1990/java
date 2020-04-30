69aafdfabd4039cdcf03c510372ca12a2dcb66c6
code1990 <s1332177151@sina.com>
2020-04-30 17:11:00 +0800

    init

--- src/test/java/AddMd.java
+++ src/test/java/AddMd.java
@@ -6,13 +6,13 @@ import java.io.IOException;
 public class AddMd {
     @Test
     public void getInfo(){
-        String path = "D:\\github\\java\\src\\main\\java\\";
+//        String path = "D:\\github\\java\\src\\main\\java\\";
+        String path = "D:\\github\\hadoop\\src\\main\\java\\";
         for(File f:new File(path).listFiles()){
             boolean flag = false;
             for(File f2:f.listFiles()){
                 if(!f2.getName().contains(".md")){
                     flag=true;
-                    break;
                 }
                 System.out.println(path+f.getName());
 //                System.out.println(f2.getName());
